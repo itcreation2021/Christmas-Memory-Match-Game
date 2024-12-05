6843776cb8efd5957b9a2e088245a669c831b9f5
itcreation2021 <itcreation.myanmar@gmail.com>
2024-12-06 04:46:38 +0630

    update responsive size

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,7 +7,7 @@ const GameBoard = () => {
     const { cards } = useContext(GameContext);
 
   return (
-    <div className="absolute top-[16rem] md:top-[15rem] grid grid-cols-4 gap-3">
+    <div className="absolute top-[16rem] sm:top-[17rem] md:top-[15rem] grid grid-cols-4 gap-3">
       {cards.map((icon, index) => (
         <IconCard key={index} iconId={index} />
       ))}
