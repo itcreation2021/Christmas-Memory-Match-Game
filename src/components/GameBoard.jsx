6d28412d897cb758269ba3840535ffb0ca2ea301
itcreation2021 <itcreation.myanmar@gmail.com>
2024-12-06 16:32:47 +0630

    update

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -8,7 +8,7 @@ const GameBoard = () => {
     const { cards } = useContext(GameContext);
 
   return (
-    <div className="gameboard absolute md:top-[15rem] grid grid-cols-4 gap-3">
+    <div className="gameboard absolute grid grid-cols-4 gap-3">
       {cards.map((icon, index) => (
         <IconCard key={index} iconId={index} />
       ))}
