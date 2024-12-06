77dc82caaaac096db4614dc48b674f6ebd6ee1cc
itcreation2021 <itcreation.myanmar@gmail.com>
2024-12-06 15:45:35 +0630

    responsive

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -2,13 +2,14 @@ import React, { useContext } from 'react'
 import RestartBtn from "../assets/images/restart-btn.png";
 import ClockBtn from "../assets/images/clock.png";
 import { GameContext } from '../context/GameContextProvider';
+import './responsive.css'
 
 const ButtonMenu = () => {
 
     const { timeLeft, restartGame } = useContext(GameContext);
 
   return (
-    <div className="absolute md:right-[10rem] top-[35rem] md:top-[20rem] flex flex-col items-start w-[18rem]">
+    <div className=" buttonmenu absolute md:right-[10rem] md:top-[20rem] flex flex-col items-start w-[18rem]">
       <div className="flex justify-center  items-center gap-3 bg-button-background bg-center bg-contain bg-no-repeat w-full h-[5rem]">
         <img src={ClockBtn} alt="" className=" h-10" />
         <p className=" text-white md:text-lg font-bold">{timeLeft} secs</p>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import IconCard from './IconCard';
 import { GameContext } from '../context/GameContextProvider';
+import './responsive.css'
 
 const GameBoard = () => {
 
     const { cards } = useContext(GameContext);
 
   return (
-    <div className="absolute top-[16rem] sm:top-[17rem] md:top-[15rem] grid grid-cols-4 gap-3">
+    <div className="gameboard absolute md:top-[15rem] grid grid-cols-4 gap-3">
       {cards.map((icon, index) => (
         <IconCard key={index} iconId={index} />
       ))}
